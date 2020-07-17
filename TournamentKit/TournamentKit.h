f9e5cb46e4c40811713bb00f603d951a10f4fb51
Jan Wittler <dev.wittler@gmail.com>
2020-07-17 12:41:27 +0200

    Initial Commit

---
+++ TournamentKit/TournamentKit.h
@@ -0,0 +1,19 @@
+//
+//  TournamentKit.h
+//  TournamentKit
+//
+//  Created by Jan Wittler on 17.07.20.
+//  Copyright © 2020 Jan Wittler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TournamentKit.
+FOUNDATION_EXPORT double TournamentKitVersionNumber;
+
+//! Project version string for TournamentKit.
+FOUNDATION_EXPORT const unsigned char TournamentKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TournamentKit/PublicHeader.h>
+
+
